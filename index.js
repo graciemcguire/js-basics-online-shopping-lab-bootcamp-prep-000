4ca3102f3154a8359c2959fdc2adbed25fbf1b28
Gracie McGuire <m.graciemcguire@gmail.com>
2018-09-21 17:50:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function placeOrder(cardNumber) {
   if (!cardNumber){
     return "Sorry, we don't have a credit card on file for you.";
   } else {
-    let 
+    let totalPrice = total();
     return 
   }
 }
