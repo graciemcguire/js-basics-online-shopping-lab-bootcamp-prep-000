10e46cee7d6fdf2c34315468fbbec2920f9b2263
Gracie McGuire <m.graciemcguire@gmail.com>
2018-09-21 17:50:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,6 +58,8 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (!cardNumber){
-    
+    return "Sorry, we don't have a credit card on file for you.";
+  } else {
+    return 
   }
 }
